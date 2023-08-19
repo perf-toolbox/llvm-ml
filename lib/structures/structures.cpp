8f39fbfd24e549ceddc7cb0f755c0c8c7aaca9d1
Alexander Batashev <alexbatashev@outlook.com>
2023-08-19 19:27:08 +0300

    fix: increase message size limit (#40)

--- lib/structures/structures.cpp
+++ lib/structures/structures.cpp
@@ -35,7 +35,7 @@ int llvm_ml::readFromFile(fs::path path,
   }
   kj::AutoCloseFd autoFd(fd);
 
-  capnp::ReaderOptions opts{.traversalLimitInWords = 128 * 1024 * 1024,
+  capnp::ReaderOptions opts{.traversalLimitInWords = 512 * 1024 * 1024,
                             .nestingLimit = 128};
 
   capnp::PackedFdMessageReader message(std::move(autoFd), opts);
