30dcd26398f919b6cb282e7af2587b769d464533
Alexander Batashev <alexbatashev@outlook.com>
2023-08-28 21:31:11 +0300

    fix: correctly retrieve cache miss event

--- tools/llvm-mc-bench/counters.cpp
+++ tools/llvm-mc-bench/counters.cpp
@@ -80,7 +80,7 @@ public:
         counters.push_back(
             CounterValue{.type = Counter::Instructions,
                          .value = static_cast<size_t>(value.value)});
-      } else if (value.name.starts_with("cache_misses")) {
+      } else if (value.name.starts_with("cache_")) {
         counters.push_back(
             CounterValue{.type = Counter::CacheMisses,
                          .value = static_cast<size_t>(value.value)});
